b3d0e818eb3685a2b6d4b07e9f4aeb0440d39310
phil11971 <phil182@mail.ru>
2018-12-01 16:40:59 +0300

    Revert "Revert "рабочий билд""
    This reverts commit ab9c69b82ed80a2602e19629dd48ed9ce5da50d4.

--- ClassicCrossword/Program.cs
+++ ClassicCrossword/Program.cs
@@ -16,7 +16,7 @@ namespace ClassicCrossword
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new StartWindow());
         }
     }
 }
