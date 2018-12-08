227698085ce46c48eee4a7acc78c531f6204c671
phil11971 <phil182@mail.ru>
2018-12-08 18:43:37 +0300

    Поменял имя формы UserWindow и проверил работу по SSH

---
+++ ClassicCrossword/Forms/UserWindowForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassicCrossword.Forms
+{
+    public partial class UserWindowForm : Form
+    {
+        public UserWindowForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
