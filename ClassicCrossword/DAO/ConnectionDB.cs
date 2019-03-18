f60d4c94cd3161f9b8d33a13a8ef21275cb65eb5
phil11971 <phil182@mail.ru>
2019-03-18 09:21:48 +0400

    подключение к БД через App.config

---
+++ ClassicCrossword/DAO/ConnectionDB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassicCrossword.DAO
+{
+    static class ConnectionDB
+    {
+        public static SqlConnection Connect()
+        {
+            SqlConnection sqlConnection = null;
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["CrosswordDBConnectionString"].ConnectionString;
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return sqlConnection;
+        }
+
+        public static void Disconnect(SqlConnection sqlConnection)
+        {
+            try
+            {
+                if (sqlConnection != null)
+                {
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
